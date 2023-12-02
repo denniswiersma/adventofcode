451c6f2947bf0cfe086e5ea078a5d54bda8e2089
denniswiersma <dennis.wiersma@icloud.com>
2023-12-02 14:56:17 +0100

    finished day1 part1

--- 2023/day01/src/bin/part1.rs
+++ 2023/day01/src/bin/part1.rs
@@ -1,12 +1,40 @@
+fn readlines(input: &str) -> Vec<String> {
+    input
+        .split("\n")
+        .map(|line| String::from(line))
+        .collect()
+}
+
+fn find_first_digit(line: String) -> Option<u32> {
+    for c in line.chars() {
+        if c.is_digit(10) {
+            return Some(c as u32 - 48)
+        }
+    }
+    None
+}
+
+fn part1(input: &str) -> u32 {
+    let mut lines = readlines(input);
+    lines.pop();
+
+    let mut total = 0;
+
+    for line in lines {
+        let first_digit = find_first_digit(line.clone()).unwrap();
+        let last_digit = find_first_digit(line.chars().rev().collect()).unwrap();
+        let full_digit = first_digit * 10 + last_digit;
+        total += full_digit;
+    }
+    total
+}
+
 fn main() {
     let input = include_str!("../../input.txt");
     let output = part1(input);
     println!("{}", output);
 }
 
-fn part1(input: &str) -> String {
-    todo!()
-}
 
 #[cfg(test)]
 mod tests {
@@ -14,8 +42,11 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let input = "put example input here";
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
 
-        assert_eq!(part1(input), "put example output here");
+        assert_eq!(part1(input), 142);
     }
 }
